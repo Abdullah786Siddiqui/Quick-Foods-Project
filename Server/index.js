588e21682536b3fd91eae51465dd13089dc0f76c
Abdullah Siddiqui <abdullahsidzz333@gmail.com>
2026-01-02 19:27:25 +0500

    push finaly

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,7 @@ const adminRoutes = require('./Routes/Admin/adminRoutes');
 const restaurantAuthRoutes = require('./Routes/Restaurant/restaurantAuthRoutes');
 
 const connectDB = require('./config/db');
-const port =  3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(express.json());
